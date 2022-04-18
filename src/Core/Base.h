39a77ebab99582ac5203fa17b6cfcd7bfb705e8d
Niels Eppenhof <n.eppenhof@gmail.com>
2022-04-18 20:16:56 +0200

    Precompiled headers

--- src/Core/Base.h
+++ src/Core/Base.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#include <iostream>
-#include <map>
-#include <string>
-#include <vector>
-
 #include "Core/PlatformDetection.h"
 
 // Platform dependent debugbreak
---
+++ src/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "Core/Base.h"
